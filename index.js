b659f12ecd6d5cf110fbc7ad301f5dd2f032ad05
ellen <ellenjojeffries@gmail.com>
2024-06-17 14:54:51 -0600

    index.js

--- index.js
+++ index.js
@@ -1,25 +1,34 @@
-// Code your solutions in this file
-function writeCards(names, eventName) {
-    let messages = [];
-  
-    for (let i = 0; i < names.length; i++) {
-      messages.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
-    }
-  
-    return messages;
-  }
-  
-  
-  const names = ["Ellen", "Jack", "Bryce"];
-  const eventName = "birthday";
+const gifts = ["teddy bear", "drone", "doll"];
 
-  function countDown(startingNumber) {
-    let count = startingNumber;
-  
-    while (count >= 0) {
-      console.log(count);
-      count--;
-    }
+function wrapGifts(gifts) {
+  for(let i = 0; i < gifts.length; i++){
+    console.log(`Wrapped ${gifts[i]} and added a bow!`)
+    
   }
-  
-  countDown(10);
+  return gifts;
+}
+
+wrapGifts(gifts)
+
+
+function writeCards(names, event) {
+  const messages = []
+  for(let i = 0;  i< names.length; i++) {
+    const message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`
+  messages.push(message)
+
+  }
+return messages;
+}
+
+const cards = writeCards(["Charlie", "Samip", "Ali",], "birthday");
+console.log(cards)
+
+
+function countDown(number) {
+while (number >= 0) {
+  console.log(number);
+  number--;
+}
+}
+countDown(10)
